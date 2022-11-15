0856912475236defdecfb6aa66f0758f0e39795b
Qingyang Li <qil77@pitt.edu>
2022-11-14 21:24:51 -0500

    log in form created

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask import url_for
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
+import click
+
 
 import os
 import sys
@@ -12,10 +14,10 @@ if WIN:
     prefix = 'sqlite:///'
 else: 
     prefix = 'sqlite:////'
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app = Flask(__name__)
 
 # class User(db.Model): 
 #     id = db.Column(db.Integer, primary_key=True) 
@@ -40,6 +42,25 @@ app = Flask(__name__)
 
 
 
+# @app.route('/')
+# @app.route('/login', methods =['GET', 'POST'])
+# def login():
+#     msg = ''
+#     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+#         username = request.form['username']
+#         password = request.form['password']
+#         # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#         # cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+#         # account = cursor.fetchone()
+#         if len(username) != 0 and len(password) != 0:
+#             # session['loggedin'] = True
+#             # session['id'] = account['id']
+#             # session['username'] = account['username']
+#             msg = 'Logged in successfully !'
+#             return redirect(url_for('index'))
+#         else:
+#             msg = 'Incorrect username / password !'
+#     return render_template('/components/login.html')
 
 @app.route('/')
 def index():
