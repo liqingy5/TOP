6af602b09597c5608da2738628312de54ab7100a
Qingyang Li <liqingy5@msu.edu>
2022-12-02 11:38:31 -0500

    Senior table set up

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session,flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask import url_for
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager,UserMixin,login_required, logout_user, login_user,current_user
+from flask_login import LoginManager, UserMixin, login_required, logout_user, login_user, current_user
 
 
 import click
@@ -16,11 +16,12 @@ import sys
 WIN = sys.platform.startswith('win')
 if WIN:
     prefix = 'sqlite:///'
-else: 
+else:
     prefix = 'sqlite:////'
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + \
+    os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'dev'
 
@@ -30,45 +31,42 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 
-
-import click
-
-class User(db.Model,UserMixin): 
-    id = db.Column(db.Integer, primary_key=True) 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))  
-    name=db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(20))
     phone = db.Column(db.String(20))
     address = db.Column(db.String(20))
     pets = db.relationship('Pet', backref='user')
+    senior = db.relationship('Senior', backref='user', uselist=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+
     def validate_password(self, password):
         return check_password_hash(self.password_hash, password)
+
 
 class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
-    type= db.Column(db.String(20))
+    type = db.Column(db.String(20))
     breed = db.Column(db.String(20))
-    age = db.Column(db.String(20))
-    weight = db.Column(db.String(20))
-    activity_level= db.Column(db.String(40))
-    food_preference= db.Column(db.String(40))
+    age = db.Column(db.Integer)
+    weight = db.Column(db.Integer)
+    activity_level = db.Column(db.Integer)
+    food_preference = db.Column(db.String(40))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-
-class Senior(db.Model):  
-    id = db.Column(db.Integer, primary_key=True)  
-    name = db.Column(db.String(20)) 
-    age = db.Column(db.Integer) 
+class Senior(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Integer)
     fav_type = db.Column(db.String(20))
-    fav_breed = db.Column(db.String(20))
-    fav_activity = db.Column(db.String(20))
-    fav_age = db.Column(db.String(20))
-    fav_weight = db.Column(db.String(20))
+    fav_activity = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class Schedule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,6 +77,7 @@ class Schedule(db.Model):
     senior_id = db.Column(db.Integer, db.ForeignKey('senior.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+
 @login_manager.user_loader
 def load_user(user_id):
     user = User.query.get(int(user_id))
@@ -87,13 +86,26 @@ def load_user(user_id):
 
 @app.cli.command()
 def forge():
-    user = User(username='1234',name='Aaron Wu',phone='(412)123-1234',address='sennot square')
+    user = User(username='1234', name='Aaron Wu',
+                phone='(412)123-1234', address='sennot square')
+    user1 = User(username='5678', name='Elon Mask',
+                 phone='(123)456-7899', address='Twitter HQ')
+    user2 = User(username='1357', name='Brad Pitt',
+                 phone='(789)123-4566', address='California')
     user.set_password('1234')
-    pet1 = Pet(name='Dobby',type='Cat',breed='Abyssinian',age='6 months',weight='7 lbs',activity_level='annoying',food_preference='raw meat',user=user)
-    pet2 = Pet(name='Yoda',type='Cat',breed='Russian Blue',age='18 months',weight='10 lbs',activity_level='quiet',food_preference='raw meat',user=user)
+    user1.set_password('5678')
+    user2.set_password('1357')
+    pet1 = Pet(name='Dobby', type='Cat', breed='Abyssinian', age=6, weight=7,
+               activity_level=1, food_preference='raw meat', user=user)
+    pet2 = Pet(name='Yoda', type='Cat', breed='Russian Blue', age=18, weight=10,
+               activity_level=3, food_preference='raw meat', user=user)
+    senior1 = Senior(age=50, fav_type="1,2,4,", fav_activity=1, user=user1)
+    senior2 = Senior(age=66, fav_type="4", fav_activity=3, user=user2)
     db.session.add(user)
     db.session.add(pet1)
     db.session.add(pet2)
+    db.session.add(senior1)
+    db.session.add(senior2)
     db.session.commit()
     click.echo('Done.')
 
@@ -108,7 +120,7 @@ def initdb(drop):
     click.echo('Initialized database.')  # 输出提示信息
 
 
-@app.route('/', methods =['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -119,13 +131,14 @@ def login():
             return redirect(url_for('login'))
 
         user = User.query.filter_by(username=username).first()
-        if user and username ==user.username and user.validate_password(password):
+        if user and username == user.username and user.validate_password(password):
             login_user(user)
             flash('Login success.')
             return redirect(url_for('index'))
         flash('Invalid username or password.')
         return redirect(url_for('login'))
     return render_template('/components/login.html')
+
 
 @app.route('/logout')
 @login_required  # 用于视图保护
@@ -134,14 +147,16 @@ def logout():
     flash('Goodbye.')
     return redirect(url_for('login'))  # 重定向回首页
 
-@app.route('/scheduleRequest',methods=['GET', 'POST'])
+
+@app.route('/scheduleRequest', methods=['GET', 'POST'])
 def scheduleRequest():
     if request.method == 'POST':
         pet = request.form['pet']
         date = request.form['date']
         droptime = request.form['dropOffTime']
         picktime = request.form['pickUpTime']
-        print('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s)', (pet, date, droptime, picktime))
+        print('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s)',
+              (pet, date, droptime, picktime))
         print(request.form['submitBtn'])
         if request.form['submitBtn'] == "The most suitable senior citizen":
             print("Pet owner chooses to match the most suitable senior")
@@ -158,24 +173,25 @@ def scheduleRequest():
         return redirect('scheduleRequest')
     return render_template('/components/schedule.html')
 
+
 @app.route('/AI_schedule')
 def AI_schedule():
     return render_template('/components/AI_schedule.html')
+
 
 @app.route('/hybrid_schedule')
 def hybrid_schedule():
     return render_template('/components/hybrid_schedule.html')
 
+
 @app.route('/manual_schedule')
 def manual_schedule():
     return render_template('/components/manual_schedule.html')
 
+
 @app.route('/confirm')
 def confirm():
     return render_template('/components/confirm.html')
-
-
-
 
 
 @app.route('/index')
@@ -183,17 +199,20 @@ def confirm():
 def index():
     return render_template('index.html')
 
+
 @app.route('/home')
 @login_required
 def home():
     return render_template('/components/home.html')
+
 
 @app.route('/schedule')
 @login_required
 def schedule():
     return render_template('/components/schedule.html')
 
-@app.route('/myInfo',methods=['GET','POST'])
+
+@app.route('/myInfo', methods=['GET', 'POST'])
 @login_required
 def myInfo():
     user = User.query.get(current_user.id)
@@ -209,10 +228,10 @@ def myInfo():
         user.address = address
         db.session.commit()
         flash('Update success.')
-    return render_template('/components/info.html',user=user)
+    return render_template('/components/info.html', user=user)
 
 
-@app.route('/petsList/delete/<int:pet_id>',methods=['GET','POST'])
+@app.route('/petsList/delete/<int:pet_id>', methods=['GET', 'POST'])
 @login_required
 def deletePet(pet_id):
     pet = Pet.query.get(pet_id)
@@ -222,7 +241,7 @@ def deletePet(pet_id):
     return redirect(url_for('petsList'))
 
 
-@app.route('/petsList/addPet',methods=['GET','POST'])
+@app.route('/petsList/addPet', methods=['GET', 'POST'])
 @login_required
 def newPet():
     if(request.method == 'POST'):
@@ -236,26 +255,28 @@ def newPet():
         if not name or not type or not breed or not age or not weight or not activity_level or not food_preference:
             flash('Invalid input.')
             return redirect(url_for('newPet'))
-        pet = Pet(name=name,type=type,breed=breed,age=age,weight=weight,activity_level=activity_level,food_preference=food_preference,user=current_user)
+        pet = Pet(name=name, type=type, breed=breed, age=age, weight=weight,
+                  activity_level=activity_level, food_preference=food_preference, user=current_user)
         db.session.add(pet)
         db.session.commit()
         flash('Add success.')
         return redirect(url_for('petsList'))
     return render_template('/components/pet.html', owner=current_user.name)
 
-@app.route('/petsList/pet/<pet_id>',methods=['GET','POST'])
+
+@app.route('/petsList/pet/<pet_id>', methods=['GET', 'POST'])
 @login_required
 def pet(pet_id):
     pet = Pet.query.get(pet_id)
-    
-    if request.method =='POST':
+
+    if request.method == 'POST':
         name = request.form['petName']
         type = request.form['petType']
-        breed= request.form['petBreed']
+        breed = request.form['petBreed']
         age = request.form['petAge']
         weight = request.form['petWeight']
-        activity_level= request.form['petActivity']
-        food_preference= request.form['petFoodPreference']
+        activity_level = request.form['petActivity']
+        food_preference = request.form['petFoodPreference']
         if not name or not type or not breed or not age or not weight or not activity_level or not food_preference:
             flash('Invalid input.')
             return redirect(url_for('pet', pet_id=pet_id))
@@ -270,17 +291,19 @@ def pet(pet_id):
         db.session.commit()
         flash('Pet info Updated.')
         return redirect(url_for('petsList'))
-    return render_template('/components/pet.html',pet=pet,owner=current_user.name)
+    return render_template('/components/pet.html', pet=pet, owner=current_user.name)
 
 # @app.route('/pet')
 # def pet():
 #     return render_template('/components/pet.html')
 
+
 @app.route('/petsList')
 @login_required
 def petsList():
     pets = User.query.get(current_user.id).pets
-    return render_template('/components/petsList.html',pets=pets)
+    return render_template('/components/petsList.html', pets=pets)
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
